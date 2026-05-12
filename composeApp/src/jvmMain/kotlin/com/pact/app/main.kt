064c467176a38f3131e2ae7bf21cdb95d1368e63
Panth Thaker <thakep3@mcmaster.ca>
2026-05-12 14:47:05 -0400

    inital project setup

---
+++ composeApp/src/jvmMain/kotlin/com/pact/app/main.kt
@@ -0,0 +1,13 @@
+package com.pact.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Pact",
+    ) {
+        App()
+    }
+}
